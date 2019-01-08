252e5930bc367f3f352c55f46b52337e4abae6fb
drdhaval2785 <drdhaval2785@gmail.com>
2019-01-08 20:51:03 +0530

    postprocess and issue1 modular

--- scripts/issue1.py
+++ scripts/issue1.py
@@ -1,6 +1,7 @@
 import codecs
 import re
 import glob
+import sys
 import os
 
 def revert_correct():
@@ -12,32 +13,39 @@ def revert_correct():
 		rep = re.sub('([^ -~उ])उ', '\g<1>=', word)
 		result[rep] = word
 	return result
+
+def correct_issue1(book, filein):
+	# Read data
+	fin = codecs.open(filein, 'r', 'utf-8')
+	data = fin.read().split('\n')
+	fin.close()
+	# Manipulate data
+	data1 = ''
+	for line in data:
+		words = line.split(' ')
+		wordCorrected = []
+		for word in words:
+			if book == 'nyasa' and re.search('[^ -~उ]उ', word):
+				rep = re.sub('([^ -~उ])उ', '\g<1>=', word)
+				for (key, value) in revertible.items():
+					rep = rep.replace(key, value)
+				wordCorrected.append(rep)
+			else:
+				wordCorrected.append(word)
+		
+		line = ' '.join(wordCorrected)
+		data1 = data1 + line + '\n'
+	data1 = data1.rstrip()
+	# Write data
+	fout = codecs.open(filein, 'w', 'utf-8')
+	fout.write(data1)
+	fout.close()
+	
 	
 if __name__ == "__main__":
+	book = sys.argv[1]
 	revertible = revert_correct()
-	filenames = glob.glob('../postprocessed/*.txt')
-	for filename in filenames:
-		print(filename)
-		fin = codecs.open(filename, 'r', 'utf-8')
-		data = fin.read().split('\n')
-		fin.close()
-		fout = codecs.open(filename, 'w', 'utf-8')
-		data1 = ''
-		for line in data:
-			words = line.split(' ')
-			wordCorrected = []
-			for word in words:
-				if re.search('[^ -~उ]उ', word):
-					rep = re.sub('([^ -~उ])उ', '\g<1>=', word)
-					for (key, value) in revertible.items():
-						rep = rep.replace(key, value)
-					wordCorrected.append(rep)
-				else:
-					wordCorrected.append(word)
-			
-			line = ' '.join(wordCorrected)
-			data1 = data1 + line + '\n'
-		data1 = data1.rstrip()
-		fout.write(data1)
-		fout.close()
-			
+	filenames = glob.glob('../' + book + '/postprocessed/*.txt')
+	for filein in filenames:
+		print(filein)
+		correct_issue1(book, filein)
--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -3,23 +3,29 @@ import re
 import glob
 import sys
 
-if __name__ == "__main__":
-	filenames = glob.glob('../output/*.txt')
-	for filename in filenames:
-		print(filename)
-		# Read data
-		fin = codecs.open(filename, 'r', 'utf-8')
-		data = fin.read()
-		fin.close()
-		# Manipulate data
+
+def postprocess_book(book, filein, fileout):
+	# Read data
+	fin = codecs.open(filein, 'r', 'utf-8')
+	data = fin.read()
+	fin.close()
+	# Do book specific postprocessing.
+	if book == 'nyasa':
 		data = data.replace('"', '`')
 		data = data.replace('््', '्')
 		data = data.replace(' उ ', ' = ')
 		data = data.replace('इ = ण्', 'इ उ ण्')
 		data = data.replace('= ऊ ', 'उ ऊ')
-		# Write data
-		fileout = filename.replace('../output', '../postprocessed')
-		fout = codecs.open(fileout, 'w', 'utf-8')
-		fout.write(data)
-		fout.close()
+	# Write data
+	fout = codecs.open(fileout, 'w', 'utf-8')
+	fout.write(data)
+	fout.close()
+
+if __name__ == "__main__":
+	book = sys.argv[1]
+	filenames = glob.glob('../' + book + '/output/*.txt')
+	for filein in filenames:
+		print(filein)
+		fileout = filein.replace('output', 'postprocessed')
+		postprocess_book(book, filein, fileout)
 	
