27087911d86ce676cc11add312027b0725c5ad25
drdhaval2785 <drdhaval2785@gmail.com>
2019-01-08 21:43:41 +0530

    fetcher from sansknet

---
+++ scripts/fetch_from_sansknet.py
@@ -0,0 +1,36 @@
+import requests
+import os
+import sys
+import re
+import codecs
+
+
+commdict = {'kashika': 'kasikavritti', 'nyasa': 'kasikavritti/nyasa', 'padamanjari': 'kasikavritti/padamanjari'}
+def fetch_pada(commentary, adhyaya, pada):
+	baseurl = 'http://www.wilbourhall.org/sansknet/vyakaranam/'
+	adhyaya = str(adhyaya)
+	pada = str(pada)
+	url = baseurl + commdict[commentary] + '/a' + adhyaya + 'p' + pada + '.htm'
+	r = requests.get(url)
+	return r.text
+
+def cleanhtml(raw_html):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
+
+def get_full_data(commentary):
+	for a in range(1,9):
+		for p in range(1,5):
+			print(a, p)
+			html_data = fetch_pada(commentary, a, p)
+			html_data = html_data.replace('<BR>', '\n')
+			text_data = cleanhtml(html_data)
+			fileout = '../' + commentary + '/orig/a' + str(a) + 'p' + str(p) + '.txt'
+			fout = codecs.open(fileout, 'w', 'utf-8')
+			fout.write(text_data)
+			fout.close()
+			
+if __name__ == "__main__":
+	commentary = sys.argv[1]
+	get_full_data(commentary)
